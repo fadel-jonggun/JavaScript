ea2a63b254d176637a65a002e8608161df0b1757
fadel-jonggun <fadel2908@student.abudzar.sch.id>
2025-01-31 09:35:19 +0700

    tugas

--- Calculator/kalkulator.js
+++ Calculator/kalkulator.js
@@ -23,3 +23,27 @@ function total(){
     let getElement = document.getElementById('hasil').innerHTML;
     console.log(getElement)
 }
+function numberThree() {
+    let getElement = document.getElementById('hasil').innerHTML;
+    console.log(getElement)
+    let tambahTiga = getElement + 3;
+    document.getElementById('hasil').innerHTML = tambahTiga;
+}
+function numberFour() {
+    let getElement = document.getElementById('hasil').innerHTML;
+    console.log(getElement)
+    let tambahEmpat= getElement + 4;
+    document.getElementById('hasil').innerHTML = tambahEmpat;
+}
+function numberFive() {
+    let getElement = document.getElementById('hasil').innerHTML;
+    console.log(getElement)
+    let tambahLima= getElement + 5;
+    document.getElementById('hasil').innerHTML = tambahLima;
+}
+function numberSix() {
+    let getElement = document.getElementById('hasil').innerHTML;
+    console.log(getElement)
+    let tambahEnam= getElement + 6;
+    document.getElementById('hasil').innerHTML = tambahEnam;
+}
