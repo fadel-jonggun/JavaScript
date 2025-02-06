b4cd8a4288e97b24bb61da4dacd58c24392bf6a5
fadel-jonggun <fadel2908@student.abudzar.sch.id>
2025-02-06 10:39:27 +0700

    tugas

---
+++ M12/tugas.js
@@ -0,0 +1,17 @@
+function penambahan(a, b) {
+    return a + b;
+    }
+     
+    function perkalian(a, b) {
+    return a * b;
+    }
+     
+    // Fungsi utama yang menerima callback
+    function calculate(num1, num2, operation) {
+        let result = operation(num1, num2);
+        console.log("Hasil:", result);
+    }
+     
+    // pemanggilan fungsi
+    calculate(5, 3, penambahan); 
+    calculate(4, 2, perkalian);
