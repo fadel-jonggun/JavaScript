bd31ba6b1d919095cd0a46f51f8300d0ce082e4e
fadel-jonggun <fadel2908@student.abudzar.sch.id>
2025-02-20 13:28:16 +0700

    tugas tealt

---
+++ M12/tugasarrow.js
@@ -0,0 +1,14 @@
+function calculateArea(length, width) {
+  return length * width;
+}
+
+//! Arrow Function
+var area = () => {
+    return length * width;
+}
+
+//! Exepression Function
+let calculate = function(length, width){
+    console.log("Hello Expression");
+}
+calculate()
